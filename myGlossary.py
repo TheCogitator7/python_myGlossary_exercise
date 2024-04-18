3caeb7d685707905fa1fc1e08d11af0895b9902a
TheCogitator7 <133551983+TheCogitator7@users.noreply.github.com>
2024-04-18 15:57:47 +0900

    Add files via upload

---
+++ myGlossary.py
@@ -0,0 +1,32 @@
+#myGlossary.py
+
+from tkinter import *
+
+#키 입력 함수
+
+#메인
+window=Tk()
+window.title("My glossary")
+
+
+#레이블 생성
+
+#텍스트 엔트리 위젯 생성
+
+#제출 버튼 추가
+
+#다른 레이블 생성
+
+#텍스트 박스 생성
+
+#dict
+myGlossary={
+    "school" : "학교",
+    "boy" : "소년",
+    "girl" : "소녀",
+    "desk" : "책상",
+    "chair" : "의자",
+}
+
+#메인 반복문 실행
+window.mainloop()
